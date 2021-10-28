97d6bfa77eeb0ae10c8f3c4479f53fc7c2f2686a
Vinicius Silva <volsilva@gmail.com>
2021-10-28 14:47:31 -0300

    Update MessagingStompWebsocketApplication.java

--- src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++ src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -2,11 +2,24 @@ package com.example.messagingstompwebsocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MessagingStompWebsocketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+			}
+		};
 	}
 }
