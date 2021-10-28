ffcacc1f2d8b84b9a3003a4b535e546e58b37213
Vinicius Silva <volsilva@gmail.com>
2021-10-28 15:10:28 -0300

    Update GreetingController.java

--- src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -3,12 +3,13 @@ package com.example.messagingstompwebsocket;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
 
-
+	@CrossOrigin
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message) throws Exception {
